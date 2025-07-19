2c047324049ae3fb17fcadb0f3e1e38c4521bef0
Petter Salminen <petter.salminen@r2m.se>
2025-07-19 11:00:11 +0200

    feat(build): improve tooling with code checks and additional dependencies
    - Added `ruff check`, `slotscheck`, and enhanced test output in `build.py`.
    - Updated development dependencies (`hatchling`, `slotscheck`) and version bumps for packages (`click`, `coverage`).

--- scripts/build.py
+++ scripts/build.py
@@ -2,7 +2,13 @@ import subprocess
 
 
 def build():
+    print("🐶🧹 Formatting code with ruff")
     subprocess.run("ruff format .", check=True, shell=True)
+    print("🐶🔬 Checking code with ruff")
+    subprocess.run("ruff check .", check=True, shell=True)
+    print("🎰 Checking __slots__ with slotscheck")
+    subprocess.run("slotscheck blood_moon/", check=True, shell=True)
+    print("🧪 Running tests with pytest")
     subprocess.run("pytest --cov=blood_moon .", check=True, shell=True)
 
 
